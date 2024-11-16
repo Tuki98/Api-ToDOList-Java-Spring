7aaea7c474072970dc9519333c1dc2eabe2e706d
GriffinTheFirst <gj.yt.17.2005@gmail.com>
2024-11-16 02:22:08 -0500

    Service e implementacion para usuario

---
+++ Avance JWT/pid_grupo01/src/main/java/com/back/pid_grupo01/service/UsuarioService.java
@@ -0,0 +1,29 @@
+package com.back.pid_grupo01.service;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.back.pid_grupo01.model.Usuario;
+import com.back.pid_grupo01.repository.UsuarioRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public Optional<Usuario> findByEmail(String email) {
+        return usuarioRepository.findByEmail(email);
+    }
+
+    public Optional<Usuario> findByDni(String dni) {
+        return usuarioRepository.findByDni(dni);
+    }
+
+    public Optional<Usuario> findByNombre(String nombre) {
+        return usuarioRepository.findByNombre(nombre);
+    }
+}
