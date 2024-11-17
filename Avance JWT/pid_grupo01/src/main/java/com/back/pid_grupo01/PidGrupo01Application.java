53fdc68b5aeca1ce995b69739e3216d719055012
Oscar Del Milagro Aquise Falcon <112704220+Tuki98@users.noreply.github.com>
2024-11-17 13:57:08 -0500

    Update PidGrupo01Application.java

--- Avance JWT/pid_grupo01/src/main/java/com/back/pid_grupo01/PidGrupo01Application.java
+++ Avance JWT/pid_grupo01/src/main/java/com/back/pid_grupo01/PidGrupo01Application.java
@@ -9,5 +9,20 @@ public class PidGrupo01Application {
 	public static void main(String[] args) {
 		SpringApplication.run(PidGrupo01Application.class, args);
 	}
+@Configuration
+public static class MyConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+            }
+        };
+    }
+}
 
 }
