44674b0495b137e424b8441ca074daca38f494f5
Oscar Del Milagro Aquise Falcon <112704220+Tuki98@users.noreply.github.com>
2024-11-17 14:09:35 -0500

    Update PidGrupo01Application.java

--- Avance JWT/pid_grupo01/src/main/java/com/back/pid_grupo01/PidGrupo01Application.java
+++ Avance JWT/pid_grupo01/src/main/java/com/back/pid_grupo01/PidGrupo01Application.java
@@ -2,6 +2,10 @@ package com.back.pid_grupo01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @SpringBootApplication
 public class PidGrupo01Application {
